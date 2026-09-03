3d91c03526c874c675c98f05c5fb2ca98998d9f7
taif-ix <taif.khan@indixpert.in>
2026-09-03 12:19:35 +0530

    del test file

--- python/web_scrap_concepts/Scrap_Html/src/Domain/scrap_paths.py
+++
@@ -1,8 +0,0 @@
-
-class Path:
-    scrap_err_log = r"D:\Repositories\2025\python\web_scrap_concepts\Scrap_Html\src\Log\log.txt"
-    npi_data_path = r"D:\Repositories\2025\python\web_scrap_concepts\Scrap_Html\src\Database\npi_data.xlsx"
-    ocr_data_path = r"D:\Repositories\2025\python\web_scrap_concepts\Scrap_Html\src\Database\ocr_output.xlsx"
-    program_run_capture  = r"D:\Repositories\2025\python\web_scrap_concepts\Scrap_Html\src\Log\task_status.txt"
-
-path_obj = Path()
